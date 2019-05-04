7430b9a5c6b2298d6f569f49e13d673d76bb556e
Vazgen Aleksanyan <vazgenaleksanyan9092@gmail.com>
2019-05-04 15:48:20 +0400

    Поменял компиляцию css

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,36 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-//const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-//const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const mqpacker = require('css-mqpacker');
 
 sass.compiler = require('node-sass');
 
 const scssFiles = [
-  './src/scss/style.scss'
+  './scss/style.scss'
 ];
 
 function styles(){
   return gulp.src(scssFiles)
               .pipe(sass().on('error', sass.logError))
-//              .pipe(concat('style.css'))
-              .pipe(autoprefixer({
-                  browsers: ['> 0.1%'],
-                  cascade: false
-              }))
+              .pipe(postcss([
+                autoprefixer({
+                    browsers: [
+                      'last 2 version',
+                      'last 7 Chrome versions',
+                      'last 10 Opera versions',
+                      'last 7 Firefox versions'
+                    ]
+                }),
+                mqpacker({sort: true}),
+              ]))
               .pipe(cleanCSS())
               .pipe(gulp.dest('./'));
-//              .pipe(browserSync.stream());
 }
 
 function watch(){
-//  browserSync.init({
-//      server: {
-//          baseDir: "./"
-//      },
-//      tunnel: true
-//  });
-  
-  gulp.watch('./src/scss/**/*.scss', styles);
-//  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('./scss/style.scss', styles);
 }
 
 gulp.task('sass:watch', watch);
